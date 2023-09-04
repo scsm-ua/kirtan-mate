4eca874c4e3c605e96c23c6176b9819360c6f4ab
Kostya Net <>
2023-09-04 13:52:30 +0300

    Scripts rework

--- scripts/songConvertor.js
+++ scripts/songConvertor.js
@@ -4,27 +4,11 @@ const path = require('path');
 const { Transform } = require('stream');
 
 /**/
+const { htmlRenderer } = require('./htmlRenderer');
 const { PATHS } = require('../scripts/constants');
 const { BUILD, SRC } = PATHS;
 
-
-/**
- * Override function for MD code blocks.
- * @type {{code(*): string}}
- */
-const renderer = {
-  code(text) {
-    const lines = text
-      .replaceAll('    ', '<span class="SongPage__space">    </span>')
-      .split(/\n/)
-      .map((line) => `<div class="SongPage__line">${line}</div>\n`)
-      .join('');
-    
-    return `<section class="SongPage__verse">\n${lines}</section>`;
-  }
-};
-
-marked.use({ renderer });
+marked.use({ renderer: htmlRenderer });
 
 /**
  *
@@ -47,6 +31,7 @@ function removeDraftMark(htmlString) {
  */
 function fillTemplate(template, content) {
   const title = content.match(/<h1[^>]*>([^<]+)<\/h1>/)[1];
+
   const paths = {
     toCss: path.relative(BUILD.HTML_FILES, BUILD.CSS_FILES),
     toIcons: path.relative(BUILD.HTML_FILES, BUILD.ICON_FILES),
